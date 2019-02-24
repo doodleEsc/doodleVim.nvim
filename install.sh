aab4cab7ab6e01e21a346aafda54b08314f3ff15
Fanlizhou <cokie@foxmail.com>
2019-02-24 16:13:10 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-
+git clone https://github.com/saibing/bingo.git
+cd bingo
+export GOPROXY=https://athens.azurefd.net/
+GO111MODULE=on go install
+mv $GOPATH/bin/bingo /usr/local/bin
+unset GOPROXY
+cd ..
+rm -rf ./bingo
