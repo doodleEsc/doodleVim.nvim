cf641e3b78336726679e755c73e2e01bb6649804
Fanlizhou <cokie@foxmail.com>
2019-02-25 00:19:11 +0800

    install pyls

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# install go language server
 git clone https://github.com/saibing/bingo.git
 cd bingo
 export GOPROXY=https://athens.azurefd.net/
@@ -8,3 +9,7 @@ mv $GOPATH/bin/bingo /usr/local/bin
 unset GOPROXY
 cd ..
 rm -rf ./bingo
+
+# install python language server
+pip3 uninstall python-language-server -y
+pip3 install 'python-language-server[all]' --upgrade
