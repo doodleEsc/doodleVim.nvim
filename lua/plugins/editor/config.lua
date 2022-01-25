08b20223135a948b1e97cbdc8f8686e00388de71
cinuor <cokie@foxmail.com>
2022-01-25 09:42:48 +0800

    fix: autopairs config

--- lua/plugins/editor/config.lua
+++ lua/plugins/editor/config.lua
@@ -82,6 +82,8 @@ function config.autopairs()
 	  map_cr = true, --  map <CR> on insert mode
 	  map_complete = true, -- it will auto insert `(` after select function or method item
 	  auto_select = true -- automatically select the first item
+	})
+end
 
 function config.blankline()
   require("indent_blankline").setup {
