458cfac8cd83fd476497a9f98b8cbbb0e4372d37
cinuor <cokie@foxmail.com>
2022-03-20 17:28:28 +0800

    fix: use main nvim-cmp

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -4,7 +4,6 @@ local conf = require('modules.completion.config')
 -- COMPLETION
 completion['hrsh7th/nvim-cmp'] = {
 	opt = true,
-	branch = "dev",
 	setup = function()
 		require("utils.defer").add("nvim-cmp", 50)
 	end,
