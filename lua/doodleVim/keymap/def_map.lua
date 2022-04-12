7577a5da6d007413d62cbb028ffbde158797c90e
cinuor <cokie@foxmail.com>
2022-04-12 18:07:47 +0800

    fix: delete and change without copy

--- lua/doodleVim/keymap/def_map.lua
+++ lua/doodleVim/keymap/def_map.lua
@@ -15,7 +15,9 @@ def_map.normal = {
   ["<C-q>"] = bind.convert_wk_format(map_cr('lua require("doodleVim.extend.misc").safe_exit()'):with_noremap():with_label("Save Session And Safe Exit")),
   ["<C-s>"] = bind.convert_wk_format(map_cr('lua require("doodleVim.extend.misc").safe_save()'):with_noremap():with_silent():with_label("Save Session")),
   ["<C-d>"] = bind.convert_wk_format(map_cr("lua require('doodleVim.extend.scroll').scroll_down()"):with_noremap():with_silent():with_label("Smooth Scroll Down")),
-  ["<C-u>"] = bind.convert_wk_format(map_cr("lua require('doodleVim.extend.scroll').scroll_up()"):with_noremap():with_silent():with_label("Smooth Scroll Up"))
+  ["<C-u>"] = bind.convert_wk_format(map_cr("lua require('doodleVim.extend.scroll').scroll_up()"):with_noremap():with_silent():with_label("Smooth Scroll Up")),
+  ["x"] = bind.convert_wk_format(map_cmd('"_x'):with_noremap():with_label("Delete Without Copy")),
+  ["c"] = bind.convert_wk_format(map_cmd('"_c'):with_noremap():with_label("Change Without Copy"))
 }
 
 def_map.insert = {
@@ -43,7 +45,10 @@ def_map.command = {
 
 def_map.visual = {
   ["<C-d>"] = bind.convert_wk_format(map_cmd("<cmd>lua require('doodleVim.extend.scroll').scroll_down()<CR>"):with_noremap():with_silent():with_label("Smooth Scroll Down"):with_mode("x")),
-  ["<C-u>"] = bind.convert_wk_format(map_cmd("<cmd>lua require('doodleVim.extend.scroll').scroll_up()<CR>"):with_noremap():with_silent():with_label("Smooth Scroll Up"):with_mode("x"))
+  ["<C-u>"] = bind.convert_wk_format(map_cmd("<cmd>lua require('doodleVim.extend.scroll').scroll_up()<CR>"):with_noremap():with_silent():with_label("Smooth Scroll Up"):with_mode("x")),
+  ["x"] = bind.convert_wk_format(map_cmd('"_d'):with_noremap():with_label("Delete Without Copy"):with_mode("x")),
+  ["c"] = bind.convert_wk_format(map_cmd('"_c'):with_noremap():with_label("Cut Without Copy"):with_mode("x"))
+
 }
 
 return def_map
