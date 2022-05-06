66644c380dc71f26e4763e42d6e9c40c94ed5850
cinuor <cokie@foxmail.com>
2022-05-06 22:57:44 +0800

    fix: extend lsp

--- lua/doodleVim/extend/lsp.lua
+++ lua/doodleVim/extend/lsp.lua
@@ -2,7 +2,7 @@ local lsp = {}
 
 local function file_and_details_entry(opts)
   opts = opts or {}
-  local displayer = ensure_require('telescope.pickers.entry_display').create {
+  local displayer = require('telescope.pickers.entry_display').create {
     separator = "│ ",
     items = {
       { width = 8 },
@@ -12,7 +12,7 @@ local function file_and_details_entry(opts)
   }
 
   local make_display = function(entry)
-    local filename = ensure_require('telescope.utils').transform_path(opts, entry.filename)
+    local filename = require('telescope.utils').transform_path(opts, entry.filename)
     local line_info = { table.concat({ entry.lnum, entry.col }, ":"), "TelescopeResultsLineNr" }
     return displayer {
       line_info,
@@ -41,22 +41,22 @@ end
 
 function lsp.lsp_references(opts)
   opts = opts or {}
-  ensure_require('telescope.builtin').lsp_references(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
+  require('telescope.builtin').lsp_references(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
 end
 
 function lsp.lsp_definitions(opts)
   opts = opts or {}
-  ensure_require('telescope.builtin').lsp_definitions(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
+  require('telescope.builtin').lsp_definitions(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
 end
 
 function lsp.lsp_type_definition(opts)
   opts = opts or {}
-  ensure_require('telescope.builtin').lsp_type_definition(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
+  require('telescope.builtin').lsp_type_definition(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
 end
 
 function lsp.lsp_implementations(opts)
   opts = opts or {}
-  ensure_require('telescope.builtin').lsp_implementations(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
+  require('telescope.builtin').lsp_implementations(vim.tbl_extend("force", opts, {entry_maker = file_and_details_entry()}))
 end
 
 return lsp
