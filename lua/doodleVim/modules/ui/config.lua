5f98b3e2cc3c67a88c4be8b1e1f6283ca9d60888
cinuor <cokie@foxmail.com>
2022-05-12 16:11:14 +0800

    feat: change gitsign text

--- lua/doodleVim/modules/ui/config.lua
+++ lua/doodleVim/modules/ui/config.lua
@@ -67,7 +67,17 @@ end
 
 function config.gitsigns()
   require('gitsigns').setup{
-    -- keymaps = {},
+    signs = {
+      add          = {hl = 'GitSignsAdd'   , text = '┃', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+      change       = {hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+      delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+      topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+      changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    },
+    signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+    numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
       interval = 2000,
       follow_files = true
