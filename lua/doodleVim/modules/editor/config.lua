be91a1fef9c989ce28e77cb09202500e2e3c7395
cinuor <cokie@foxmail.com>
2022-05-20 10:22:13 +0800

    fix: font fit all

--- lua/doodleVim/modules/editor/config.lua
+++ lua/doodleVim/modules/editor/config.lua
@@ -1,24 +1,25 @@
 local config = {}
 
 function config.todo()
+    local icons = require("doodleVim.utils.icons")
     require("todo-comments").setup {
         signs = true, -- show icons in the signs column
         sign_priority = 8, -- sign priority
         -- keywords recognized as todo comments
         keywords = {
             FIX = {
-                icon = " ", -- icon used for the sign, and in search results
+                icon = icons.todo.fix, -- icon used for the sign, and in search results
                 color = "error", -- can be a hex color, or a named color (see below)
                 alt = {
                     "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR",
                 }, -- a set of other keywords that all map to this FIX keywords
                 -- signs = false, -- configure signs for some keywords individually
             },
-            TODO = { icon = " ", color = "info" },
-            HACK = { icon = " ", color = "warning" },
-            WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-            PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-            NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+            TODO = { icon = icons.todo.todo, color = "info" },
+            HACK = { icon = icons.todo.hack, color = "warning" },
+            WARN = { icon = icons.todo.warn, color = "warning", alt = { "WARNING", "XXX" } },
+            PERF = { icon = icons.todo.perf, color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            NOTE = { icon = icons.todo.note, color = "hint", alt = { "INFO" } },
         },
         merge_keywords = true, -- when true, custom keywords will be merged with the defaults
         -- highlighting of the line containing the todo comment
@@ -27,7 +28,7 @@ function config.todo()
         -- * after: highlights after the keyword (todo text)
         highlight = {
             before = "", -- "fg" or "bg" or empty
-            keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+            keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
             after = "fg", -- "fg" or "bg" or empty
             pattern = [[.*<(KEYWORDS)\v(\s+\(.*\)|:)+]], -- pattern or table of patterns, used for highlightng (vim regex)
             -- pattern = [[.*<(KEYWORDS)\v(\s|:)+]], -- pattern or table of patterns, used for highlightng (vim regex)
@@ -172,8 +173,8 @@ end
 function config.gitsigns()
     require('gitsigns').setup {
         signs                        = {
-            add          = { hl = 'GitSignsAdd', text = '▐', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-            change       = { hl = 'GitSignsChange', text = '▐', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+            add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+            change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
             delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
             topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
             changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
