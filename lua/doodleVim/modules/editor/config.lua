4cadf46bb8ea32984c98186b355da412ef72cc2a
cinuor <cokie@foxmail.com>
2022-05-26 17:01:25 +0800

    fix: use telescope to list buffers

--- lua/doodleVim/modules/editor/config.lua
+++ lua/doodleVim/modules/editor/config.lua
@@ -266,22 +266,4 @@ function config.mkdnflow()
     })
 end
 
-function config.jabs()
-    require 'jabs'.setup {
-        -- Options for the main window
-        position = 'center', -- center, corner. Default corner
-        width = 80, -- default 50
-        height = 20, -- default 10
-        border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
-
-        -- Options for preview window
-        preview_position = 'left', -- top, bottom, left, right. Default top
-        preview = {
-            width = 40, -- default 70
-            height = 60, -- default 30
-            border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
-        },
-    }
-end
-
 return config
