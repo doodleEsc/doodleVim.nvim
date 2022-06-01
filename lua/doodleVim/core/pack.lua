e3a8ad54ad52b8113ba185c24e1439686832088c
cinuor <cokie@foxmail.com>
2022-06-01 09:36:21 +0800

    feat: packer depth 1

--- lua/doodleVim/core/pack.lua
+++ lua/doodleVim/core/pack.lua
@@ -74,7 +74,7 @@ function Packer:init_ensure_plugins()
     local packer_dir = data_dir .. 'pack/packer/opt/packer.nvim'
     local state = uv.fs_stat(packer_dir)
     if not state then
-        local cmd = "!git clone https://github.com/wbthomason/packer.nvim " .. packer_dir
+        local cmd = "!git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. packer_dir
         api.nvim_command(cmd)
         self:load_packer()
         packer.install()
