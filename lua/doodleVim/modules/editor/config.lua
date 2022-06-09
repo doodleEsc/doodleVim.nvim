8fce0b24041b6ddae167c8841635f1bd7ca5954a
cinuor <cokie@foxmail.com>
2022-06-09 16:59:42 +0800

    feat: change TODO format

--- lua/doodleVim/modules/editor/config.lua
+++ lua/doodleVim/modules/editor/config.lua
@@ -30,7 +30,7 @@ function config.todo()
             before = "", -- "fg" or "bg" or empty
             keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
             after = "fg", -- "fg" or "bg" or empty
-            pattern = [[.*<(KEYWORDS)\v(\s+\(.*\)|:)+]], -- pattern or table of patterns, used for highlightng (vim regex)
+            pattern = [[.*<(KEYWORDS)\v(\s?\(.*\)|:)+]], -- pattern or table of patterns, used for highlightng (vim regex)
             -- pattern = [[.*<(KEYWORDS)\v(\s|:)+]], -- pattern or table of patterns, used for highlightng (vim regex)
             comments_only = true, -- uses treesitter to match keywords in comments only
             max_line_len = 400, -- ignore lines longer than this
@@ -57,7 +57,7 @@ function config.todo()
             },
             -- regex that will be used to match keywords.
             -- don't replace the (KEYWORDS) placeholder
-            pattern = [[\b(KEYWORDS)(\s+\(.*\)|:)+]], -- ripgrep regex
+            pattern = [[\b(KEYWORDS)(\s?\(.*\)|:)+]], -- ripgrep regex
         },
     }
 end
