e004be8ac9a7cf688cc660513989eb988d3f631f
cinuor <cokie@foxmail.com>
2022-06-13 16:35:29 +0800

    feat: jq to format json

--- lua/doodleVim/modules/completion/config.lua
+++ lua/doodleVim/modules/completion/config.lua
@@ -227,8 +227,7 @@ function config.null_ls()
         on_init = nil,
         on_exit = nil,
         sources = {
-            null_ls.builtins.formatting.prettier,
-            null_ls.builtins.formatting.black,
+            null_ls.builtins.formatting.jq,
             null_ls.builtins.code_actions.gitsigns,
         },
         update_in_insert = false,
