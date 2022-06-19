14c78e6a2bcc2d0eb2e555f3a16d1555fb8ae93e
cinuor <cokie@foxmail.com>
2022-06-19 13:38:19 +0800

    fix: update nvim-dap-ui

--- lua/doodleVim/modules/debug/config.lua
+++ lua/doodleVim/modules/debug/config.lua
@@ -10,52 +10,56 @@ function config.dapui()
             remove = "d",
             edit = "e",
             repl = "r",
+            toggle = "t"
         },
-        sidebar = {
-            -- You can change the order of elements in the sidebar
-            elements = {
-                -- Provide as ID strings or tables with "id" and "size" keys
-                { id = "scopes", size = 0.25 },
-                { id = "breakpoints", size = 0.25 },
-                { id = "stacks", size = 0.25 },
-                { id = "watches", size = 0.25 },
+        expand_lines = vim.fn.has("nvim-0.7"),
+        layouts = {
+            {
+                elements = {
+                    -- Elements can be strings or table with id and size keys.
+                    { id = "scopes", size = 0.25 },
+                    "breakpoints",
+                    "stacks",
+                    "watches",
+                },
+                size = 40,
+                position = "left",
             },
-            size = 40,
-            position = "left", -- Can be "left", "right", "top", "bottom"
-        },
-        tray = {
-            elements = { "repl" },
-            size = 10,
-            position = "bottom", -- Can be "left", "right", "top", "bottom"
+            {
+                elements = {
+                    "repl",
+                    "console",
+                },
+                size = 10,
+                position = "bottom",
+            },
         },
         floating = {
-            max_height = 0.4, -- These can be integers or a float between 0 and 1.
-            max_width = 0.4, -- Floats will be treated as percentage of your screen.
-            border = "single", -- Border style. Can be "single", "double" or "rounded"
+            max_height = nil, -- These can be integers or a float between 0 and 1.
+            max_width = nil, -- Floats will be treated as percentage of your screen.
+            border = "rounded", -- Border style. Can be "single", "double" or "rounded"
             mappings = {
                 close = { "q", "<Esc>" },
             },
         },
         windows = { indent = 1 },
+        render = {
+            max_type_length = nil, -- Can be integer or nil.
+        }
     })
 
     local dap, dapui = require "dap", require "dapui"
     dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
-        -- vim.api.nvim_command("DapVirtualTextEnable")
-        -- dapui.close("tray")
     end
     dap.listeners.before.event_terminated["dapui_config"] = function()
-        -- vim.api.nvim_command("DapVirtualTextDisable")
         dapui.close()
     end
     dap.listeners.before.event_exited["dapui_config"] = function()
-        -- vim.api.nvim_command("DapVirtualTextDisable")
         dapui.close()
     end
     -- for some debug adapter, terminate or exit events will no fire, use disconnect reuest instead
     dap.listeners.before.disconnect["dapui_config"] = function()
-        -- vim.api.nvim_command("DapVirtualTextDisable")
         dapui.close()
     end
 end
