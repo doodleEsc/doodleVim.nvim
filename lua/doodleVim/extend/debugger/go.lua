5c7eec5cb2eec56790e4307072e24743f25b89fc
cinuor <cokie@foxmail.com>
2022-07-10 13:50:06 +0800

    chore: remove useless comment

--- lua/doodleVim/extend/debugger/go.lua
+++ lua/doodleVim/extend/debugger/go.lua
@@ -36,40 +36,6 @@ local adapter_factory = function(adapter_bin_path)
 	end
 end
 
--- go.adapters = function(callback, config)
--- 	local stdout = vim.loop.new_pipe(false)
--- 	local handle
--- 	local pid_or_err
--- 	local host = config.host or "127.0.0.1"
--- 	local port = config.port or "38697"
--- 	local addr = string.format("%s:%s", host, port)
--- 	local opts = {
--- 		stdio = { nil, stdout },
--- 		args = { "dap", "-l", addr },
--- 		detached = true,
--- 	}
--- 	handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
--- 		stdout:close()
--- 		handle:close()
--- 		if code ~= 0 then
--- 			print("dlv exited with code", code)
--- 		end
--- 	end)
--- 	assert(handle, "Error running dlv: " .. tostring(pid_or_err))
--- 	stdout:read_start(function(err, chunk)
--- 		assert(not err, err)
--- 		if chunk then
--- 			vim.schedule(function()
--- 				require("dap.repl").append(chunk)
--- 			end)
--- 		end
--- 	end)
--- 	-- Wait for delve to start
--- 	vim.defer_fn(function()
--- 		callback({ type = "server", host = "127.0.0.1", port = port })
--- 	end, 100)
--- end
-
 go.configurations = {
 	{
 		type = "go",
