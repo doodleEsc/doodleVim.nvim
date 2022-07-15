e16bee595086f39ec8d7019c2199fa5845fcc8d5
cinuor <cokie@foxmail.com>
2022-07-15 16:39:22 +0800

    feat: auto install

--- lua/doodleVim/modules/ui/config.lua
+++ lua/doodleVim/modules/ui/config.lua
@@ -30,7 +30,7 @@ function config.treesitter()
             "yaml",
         },
         sync_install = false,
-        auto_install = false,
+        auto_install = true,
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
