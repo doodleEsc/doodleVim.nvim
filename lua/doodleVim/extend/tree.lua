68e55d57f2476e308526bc11a6e4b49c8d253115
cinuor <cokie@foxmail.com>
2022-08-06 09:25:00 +0800

    feat: use nvim-tree api

--- lua/doodleVim/extend/tree.lua
+++ lua/doodleVim/extend/tree.lua
@@ -1,19 +1,13 @@
 local tree = {}
 
 tree.toggle = function()
+    if vim.bo.filetype == 'alpha' then
+        return
+    end
+
     require('doodleVim.utils.defer').immediate_load({ 'barbar.nvim', 'nvim-tree.lua' })
     local api = require('nvim-tree.api')
     api.tree.toggle()
-    -- local view = require('nvim-tree.view')
-    -- local api = require('nvim-tree.api')
-    --
-    -- if view.is_visible() then
-    --     require('bufferline.state').set_offset(0)
-    --     api.tree.close()
-    -- else
-    --     require('bufferline.state').set_offset(30, 'File Explorer')
-    --     api.tree.open()
-    -- end
 end
 
 return tree
