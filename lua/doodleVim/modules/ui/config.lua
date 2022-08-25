b7d341fbd0b1af1b34f84d68538d277e41981469
cinuor <cokie@foxmail.com>
2022-08-25 14:56:54 +0800

    fix: enable treesitter height

--- lua/doodleVim/modules/ui/config.lua
+++ lua/doodleVim/modules/ui/config.lua
@@ -34,7 +34,7 @@ function config.treesitter()
         sync_install = false,
         auto_install = true,
         highlight = {
-            enable = false,
+            enable = true,
             additional_vim_regex_highlighting = false,
         },
         textobjects = {
