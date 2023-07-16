83b2b127f9e872d1d75f4c764b3bfe934f0f66c9
doodleEsc <cinuor@gmail.com>
2023-07-16 22:37:25 +0800

    chore: remove pyinstall

--- lua/doodleVim/modules/lsp/setup.lua
+++ lua/doodleVim/modules/lsp/setup.lua
@@ -31,12 +31,12 @@ function lsp.mason(plugin)
             vim.cmd("MasonInstall" .. bins)
         end
 
-        local trim = require("doodleVim.utils.utils").trim
+        -- local trim = require("doodleVim.utils.utils").trim
         local jdtls_path = require("mason-core.path").package_prefix("jdtls")
         local lombok_jar = jdtls_path .. "/plugins/" .. "lombok.jar"
 
         -- pylsp install dependencies;
-        vim.cmd("PylspInstall python-lsp-black pyls-isort python-lsp-ruff pylsp-rope")
+        -- vim.cmd("PylspInstall python-lsp-black pyls-isort python-lsp-ruff pylsp-rope")
 
         -- check lombok and install
         if not vim.loop.fs_stat(lombok_jar) then
