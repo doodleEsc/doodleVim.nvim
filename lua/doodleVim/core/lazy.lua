2876808c8d770f6d6584f63e73cb94946e91321a
doodleEsc <cinuor@gmail.com>
2023-12-05 09:38:11 +0800

    feat: update startup event

--- lua/doodleVim/core/lazy.lua
+++ lua/doodleVim/core/lazy.lua
@@ -67,7 +67,8 @@ function Lazy:load_lazy(install)
                 -- The default is to disable on:
                 --  * VimEnter: not useful to cache anything else beyond startup
                 --  * BufReadPre: this will be triggered early when opening a file from the command line directly
-                disable_events = { "VimEnter", "BufReadPre" },
+                -- disable_events = { "VimEnter", "BufReadPre" },
+                disable_events = {},
                 ttl = 3600 * 24 * 5, -- keep unused modules for up to 5 days
             },
             reset_packpath = true,   -- reset the package path to improve startup time
